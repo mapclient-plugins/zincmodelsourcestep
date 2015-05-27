5bf6a38ae57bb44d7ead9fe259f803450648b104
Hugh Sorby <h.sorby@auckland.ac.nz>
2015-05-27 22:18:58 +1200

    Delete zincdatasourcestep, add new format zincmodelsourcestep

---
+++ mapclientplugins/zincmodelsourcestep/__init__.py
@@ -0,0 +1,13 @@
+
+'''
+MAP Client Plugin
+'''
+
+__version__ = '0.1.0'
+__author__ = 'Hugh Sorby'
+__stepname__ = 'Zinc Model Source'
+__location__ = 'https://github.com/mapclient-plugins/zincdatasourcestep'
+
+# import class that derives itself from the step mountpoint.
+from mapclientplugins.zincmodelsourcestep import step
+
