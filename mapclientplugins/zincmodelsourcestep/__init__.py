59e54ba301aaf929f7674293126b0a069c84005f
TimmyNic <tim.nicolas@outlook.com>
2023-01-25 12:14:10 +1300

    Set version to 0.1.1

--- mapclientplugins/zincmodelsourcestep/__init__.py
+++ mapclientplugins/zincmodelsourcestep/__init__.py
@@ -2,7 +2,7 @@
 MAP Client Plugin
 '''
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Zinc Model Source'
 __location__ = 'https://github.com/mapclient-plugins/zincdatasourcestep'
